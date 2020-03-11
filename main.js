23c04074ad715ffdc7c3ff1de28e2b150db26257
yanjintao25 <yanjintaojj@163.com>
2020-03-11 17:38:55 +0800

    test

---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function compare_poker(x){
+    return 0;
+}
+
+module.exports = compare_poker;
---
+++ main.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const compare_poker = require('./main.js');
+
+TextDecoderStream('test "Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH" result', 
+    () =>{
+    expect(compare_poker('Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH')).toBe('White wins');
+})
+
+TextDecoderStream('test "Black: 2H 4S 4C 2D 4H White: 2S 8S AS QS 3S" result', 
+    () =>{
+    expect(compare_poker("Black: 2H 4S 4C 2D 4H White: 2S 8S AS QS 3S")).toBe('Black wins');
+})
+
+TextDecoderStream('test "Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C KH" result', 
+    () =>{
+    expect(compare_poker("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C KH")).toBe('Black wins');
+})
+
+TextDecoderStream('test "Black: 2H 3D 5S 9C KD White: 2D 3H 5C 9S KH" result', 
+    () =>{
+    expect(compare_poker("Black: 2H 3D 5S 9C KD White: 2D 3H 5C 9S KH")).toBe('Tie');
+})
